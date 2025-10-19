f16f419cdd134343f0aff85158bf82ef5eb5c61e
Will Bolton <git@bolton.me.uk>
2025-10-19 20:51:28 +0100

    Add file parsing failure tests

--- test.py
+++ test.py
@@ -21,12 +21,26 @@ class TestFileParsing(unittest.TestCase):
         self.assertIsInstance(result, list)
         self.assertEqual(len(result), 8)
 
+    def test_parse_bad_midatas(self):
+        infiles = [f for f in os.listdir(TEST_DATA_DIR) if f.startswith('bad-midata') and f.endswith('.csv')]
+        for file in infiles:
+            with self.assertRaises(ValueError):
+                result = read_nationwide_file(os.path.join(TEST_DATA_DIR, file))
+                self.assertIsNone(result)
+
     def test_parse_statement(self):
         infile = os.path.join(TEST_DATA_DIR, "test-statement.csv")
         result = read_nationwide_file(infile)
 
         self.assertIsInstance(result, list)
         self.assertEqual(len(result), 6)
+
+    def test_parse_bad_statements(self):
+        infiles = [f for f in os.listdir(TEST_DATA_DIR) if f.startswith('bad-statement') and f.endswith('.csv')]
+        for file in infiles:
+            with self.assertRaises(ValueError):
+                result = read_nationwide_file(os.path.join(TEST_DATA_DIR, file))
+                self.assertIsNone(result)
 
 class TestFileTransactions(unittest.TestCase):
     def test_statement_consistency(self):
